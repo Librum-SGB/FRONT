9b9f22bbe0f0103706640465a9f66e1681e0b57c
marcos_p_medeiros <marcospaulobitt@gmail.com>
2026-03-16 20:23:00 -0300

    feat : Criação das item-lista-tarefas, criação da classe dashboard para deixar alinhado, var do item lista tarefa alterado de bg para classes

--- Librum/src/app/componentes/card-informativo/card-informativo.ts
+++ Librum/src/app/componentes/card-informativo/card-informativo.ts
@@ -1,15 +1,14 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { ɵEmptyOutletComponent } from '@angular/router';
 
 @Component({
   selector: 'app-card-informativo',
-  imports: [NgClass, ɵEmptyOutletComponent],
+  imports: [NgClass],
   templateUrl: './card-informativo.html',
   styleUrl: './card-informativo.scss',
 })
 export class CardInformativo {
-  @Input({ required: false }) bg: string = 'bg-success';
+  @Input({ required: false }) classe: string = 'bg-success';
   @Input({ required: true }) titulo: string = '';
   @Input({ required: true }) valor: string = '';
 }
