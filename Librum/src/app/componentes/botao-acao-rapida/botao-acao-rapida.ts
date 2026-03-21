b05b247e7c41b65f87484a58be5e4955e51d5993
marcos_p_medeiros <marcospaulobitt@gmail.com>
2026-03-20 21:14:29 -0300

    feat: add as cores dos botões

--- Librum/src/app/componentes/botao-acao-rapida/botao-acao-rapida.ts
+++ Librum/src/app/componentes/botao-acao-rapida/botao-acao-rapida.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-botao-acao-rapida',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './botao-acao-rapida.html',
   styleUrl: './botao-acao-rapida.scss',
 })
 export class BotaoAcaoRapida {
   @Input({ required: true }) titulo: string = 'titulo';
+  @Input({ required: false }) classe: string = 'bg-primary';
 }
