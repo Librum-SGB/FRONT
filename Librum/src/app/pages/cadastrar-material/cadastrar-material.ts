81a31033cd81a950b25766703d2a6e4c08489495
lauraabress <laurabrssn@gmail.com>
2026-04-06 19:47:39 -0300

    feat: criação cadastrar-material e add rotas

---
+++ Librum/src/app/pages/cadastrar-material/cadastrar-material.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-material',
+  imports: [],
+  templateUrl: './cadastrar-material.html',
+  styleUrl: './cadastrar-material.scss',
+})
+export class CadastrarMaterial {}
