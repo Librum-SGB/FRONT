9861a1e763da72e0ae90baf4f4cab72acf1a0b4b
Marcos <marcospaulobitt@gmail.com>
2026-04-26 12:54:44 -0300

    feat : tiposd de relatórios

--- Librum/src/app/pages/relatorio/relatorio.ts
+++ Librum/src/app/pages/relatorio/relatorio.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TipoRelatorio } from '../../enum/tipo-relatorio.enum';
 
 @Component({
   selector: 'app-relatorio',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './relatorio.html',
   styleUrl: './relatorio.scss',
 })
-export class Relatorio {}
+export class Relatorio {
+  selectedTipos: number[] = [];
+
+  tiposRelatorio = [
+    { id: TipoRelatorio.Usuario, nome: 'Usuário' },
+    { id: TipoRelatorio.Emprestimo, nome: 'Empréstimo' },
+    { id: TipoRelatorio.Acervo, nome: 'Acervo' },
+    { id: TipoRelatorio.Bloqueio, nome: 'Bloqueio' },
+    { id: TipoRelatorio.Devolucao, nome: 'Devoluções' },
+    { id: TipoRelatorio.Reservas, nome: 'Reservas' },
+    { id: TipoRelatorio.Penalidades, nome: 'Penalidades' },
+    { id: TipoRelatorio.Estatisticas, nome: 'Estatísticas' },
+  ];
+
+  onCheckboxChange(event: any, id: number) {
+    if (event.target.checked) {
+      this.selectedTipos.push(id);
+    } else {
+      this.selectedTipos = this.selectedTipos.filter((t) => t !== id);
+    }
+  }
+}
