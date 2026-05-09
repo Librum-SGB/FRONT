4294bfbe46aa644ba2ce3096ce518a6da2d74575
MetalHeadW <mateusdwendhausen@gmail.com>
2026-05-08 21:58:15 -0300

    Footer em andamento

--- Librum/src/app/shared/component/footer/footer.ts
+++ Librum/src/app/shared/component/footer/footer.ts
@@ -1,14 +1,90 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { LinkPersonalizado } from '../link-personalizado/link-personalizado';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-footer',
-  imports: [LinkPersonalizado],
+  imports: [LinkPersonalizado, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {
-  
+export class Footer implements OnInit {
 
+  showDucks = false;
+
+  private secretWord = '';
+
+  private isHoldingDown = false;
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+
+    window.addEventListener('keydown', (event) => {
+
+      // Detecta seta para baixo
+
+      if (event.key === 'ArrowDown') {
+
+        this.isHoldingDown = true;
+
+        return;
+      }
+
+      // Enquanto segura ↓
+
+      if (this.isHoldingDown) {
+
+        this.secretWord += event.key.toLowerCase();
+
+        // Ativa os patinhos
+
+        if (this.secretWord.includes('pato')) {
+
+          this.showDucks = true;
+
+          // Atualiza a tela instantaneamente
+
+          this.cdr.detectChanges();
+
+          console.log('🦆 Patinhos ativados');
+          setTimeout(() => {
+
+            this.showDucks = false;
+
+            this.cdr.detectChanges();
+
+          }, 12000);
+
+          this.secretWord = '';
+        }
+
+        // Limita tamanho
+
+        if (this.secretWord.length > 10) {
+
+          this.secretWord = '';
+        }
+
+      }
+
+    });
+
+    window.addEventListener('keyup', (event) => {
+
+      // Soltou a seta ↓
+
+      if (event.key === 'ArrowDown') {
+
+        this.isHoldingDown = false;
+
+        this.secretWord = '';
+      }
+
+    });
+
+  }
 }
